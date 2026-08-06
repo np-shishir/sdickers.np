d62cd98854ad85085be4ed49ce3a071f93ad917e
naiduu <shishir.404@gmail.com>
2026-08-06 07:25:57 +0545

    /marketplace integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,11 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+<<<<<<< HEAD
 import MarketPlace from './pages/MarketPlace'
+=======
+import Marketplace from './pages/Marketplace'
+>>>>>>> 484ae2f (/marketplace integration)
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +22,11 @@ function App() {
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
+<<<<<<< HEAD
         <Route path="/marketplace" element={<MarketPlace/>}/>
+=======
+        <Route path="/marketplace" element={<Marketplace/>}/>
+>>>>>>> 484ae2f (/marketplace integration)
       </Routes>
       <Footer/>
     </>
--- frontend/src/cards/TrendingCollections.jsx
+++ frontend/src/cards/TrendingCollections.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-export default function TrendingCollections({bgImage, title}) {
+export default function TrendingCollections({ bgImage, title }) {
   return (
-                        <div className='w-60 h-80 bg-contain rounded-2xl flex flex-col justify-end items-start relative cursor-pointer hover:scale-y-110 duration-500 ease-in-out' style={{backgroundImage: `url(${bgImage})`}}>
-                            <div className='absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-black/80 via-black/40 to-transparent'></div>
-                            <div className='text-white font-bold px-4 py-2 text-2xl z-10'>{title}</div>
-                        </div>
-  )
+    <div
+      className="w-60 h-80 bg-cover bg-center rounded-2xl flex flex-col justify-end relative cursor-pointer hover:scale-105 duration-300"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-black/80 via-black/40 to-transparent"></div>
+
+      <div className="text-white font-bold text-2xl px-4 py-2 z-10">
+        {title}
+      </div>
+    </div>
+  );
 }
