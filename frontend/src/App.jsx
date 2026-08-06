dd1ca9ef18a0fd77519761c6298f117f4fcc5701
naiduu <shishir.404@gmail.com>
2026-08-06 07:34:23 +0545

    integrate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,9 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-<<<<<<< HEAD
-import MarketPlace from './pages/MarketPlace'
-=======
+
 import Marketplace from './pages/Marketplace'
->>>>>>> 484ae2f (/marketplace integration)
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,11 +20,7 @@ function App() {
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
-<<<<<<< HEAD
         <Route path="/marketplace" element={<MarketPlace/>}/>
-=======
-        <Route path="/marketplace" element={<Marketplace/>}/>
->>>>>>> 484ae2f (/marketplace integration)
       </Routes>
       <Footer/>
     </>
